b53f3a1f299e9da5b774dabad6797660486d06a3
Dani Latorre <dani@danilat.com>
2016-10-21 11:39:50 +0200

    connection to the database

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hi!')
+var pg = require('pg');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/fasten'
+//pg.defaults.ssl = true;
+pg.connect(DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres!');
+
+  client
+    .query('SELECT id, body FROM iterations;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
